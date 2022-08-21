02b9886698f49c19b9271d95b01408bb7f032130
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-08-21 14:34:34 +0200

    add grate 4

---
+++ pages/primarygrade/grade4/advancedVideos.js
@@ -0,0 +1,23 @@
+import Nav from "../../../components/general/Nav";
+import IntroService from "../../../components/general/IntroService";
+import VideoPlayer from "../../../components/general/VideoPlayer";
+import Footer from "../../../components/general/Footer";
+
+const AdvancedVideo = () => {
+  return (
+    <section>
+      <div className="container mx-auto px-5 sm:px-10">
+        <Nav />
+        <IntroService
+          img="https://res.cloudinary.com/dirbnpgsp/image/upload/v1660789652/casual-life-3d-boy-and-girl-working-on-computer_mgekvh.png"
+          title="الفديوهات مفتوحة المصدر"
+          subtitle="يوجد افضل الفديوهات مفتوحة المصدر الي هذه المرحلة"
+        />
+        <VideoPlayer urlApi="primaryGrade/grade4/advancedVideos" />
+      </div>
+      <Footer />
+    </section>
+  );
+};
+
+export default AdvancedVideo;
