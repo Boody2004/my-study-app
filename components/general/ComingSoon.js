6e1ce505d15e9605a467872d89a7dd83a75883f8
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-08-22 01:17:44 +0200

    edit

--- components/general/ComingSoon.js
+++
@@ -1,9 +0,0 @@
-const ComingSoon = () => {
-  return (
-    <span className="text-white bg-lightSecondary text-sm font-medium mx-2 p-1 rounded-md">
-      قريبا
-    </span>
-  );
-};
-
-export default ComingSoon;
--- components/gettingStarted/Service.js
+++ components/gettingStarted/Service.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import IntroService from "../general/IntroService";
-import ComingSoon from "../general/ComingSoon";
 
 const Service = (props) => {
   const [items, setItems] = useState([]);
@@ -38,7 +37,6 @@ const Service = (props) => {
                 <div className="p-6">
                   <h1 className="text-2xl font-bold	text-textPrimary mb-2">
                     {item.title}
-                    {item.comingSoon ? <ComingSoon /> : ""}
                   </h1>
                   <p className="text-base	font-medium	text-light mb-1">
                     {item.underTitle}
