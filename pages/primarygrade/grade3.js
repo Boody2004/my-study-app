aede1f09ce335e6a70835efd512f305bb1b1266a
Abd Elrahman Atef <65117957+Boody2004@users.noreply.github.com>
2022-08-21 14:26:03 +0200

    add grade 3

--- pages/primarygrade/grade3.js
+++ pages/primarygrade/grade3.js
@@ -3,19 +3,19 @@ import Nav from "../../components/general/Nav";
 import ServicesGrate from "../../components/general/ServicesGrade";
 import Footer from "../../components/general/Footer";
 
-const PrimaryGrade2 = () => {
+const PrimaryGrade3 = () => {
   return (
     <section>
       <Head>
-        <title>Grade 2 | Online Lessons App</title>
+        <title>Grade 3 | Online Lessons App</title>
       </Head>
       <div className="container mx-auto px-5 sm:px-10">
         <Nav />
         <ServicesGrate
           img="https://res.cloudinary.com/dirbnpgsp/image/upload/v1660638783/casual-life-3d-boy-sitting-at-the-desk-with-open-book_qma796.png"
-          title="المرحلة الابتدائية Grade 2"
+          title="المرحلة الابتدائية Grade 3"
           subtitle="يوجد جميع كتب الوزارة و الكتب الخارجية و الفديوهات التعليمية و الفديوهات التعليمية ذات مصدر مفتوح الي هذه المرحلة"
-          urlApi="primaryGrade/grade2/grade2List"
+          urlApi="primaryGrade/grade3/grade3List"
         />
       </div>
       <Footer />
@@ -23,4 +23,4 @@ const PrimaryGrade2 = () => {
   );
 };
 
-export default PrimaryGrade2;
+export default PrimaryGrade3;
---
+++ pages/primarygrade/grade3/videos.js
@@ -0,0 +1,23 @@
+import Nav from "../../../components/general/Nav";
+import IntroService from "../../../components/general/IntroService";
+import VideoPlayer from "../../../components/general/VideoPlayer";
+import Footer from "../../../components/general/Footer";
+
+const Video = () => {
+  return (
+    <section>
+      <div className="container mx-auto px-5 sm:px-10">
+        <Nav />
+        <IntroService
+          img="https://res.cloudinary.com/dirbnpgsp/image/upload/v1660770852/casual-life-3d-young-man-and-woman-looking-at-tablet_gs0xsm.png"
+          title="الفديوهات التعليمية"
+          subtitle="يوجد افضل الفديوهات شرح الدروس الي هذه المرحلة"
+        />
+        <VideoPlayer urlApi="primaryGrade/grade3/videos" />
+      </div>
+      <Footer />
+    </section>
+  );
+};
+
+export default Video;
